b47f96f975b47372a42c82e31ae7071e1d04299f
phunghoangvnuit <phunghoangvnuit@gmail.com>
2025-03-23 09:09:39 +0700

    add smart contract

---
+++ hardhat.config.js
@@ -0,0 +1,10 @@
+require("@nomiclabs/hardhat-ethers");
+
+module.exports = {
+  solidity: "0.8.28",
+  network: {
+    hardhat: {
+      chainId: 1337,
+    },
+  },
+};
---
+++ pages/allowd-voters.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const allowdVoters = () => {
+  return <div>allowdVoters</div>;
+};
+
+export default allowdVoters;
